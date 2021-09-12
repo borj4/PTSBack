8f246694e3c39894c65e41fb10af31ad67b3f547
Borj4 <river4car@gmail.com>
2021-09-12 11:59:24 +0200

    security bugs fixed

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,15 @@
 const Sequelize = require('sequelize');
-const dotenv = require('dotenv').config();
+require('dotenv').config({path: ('../../.env')});
 
 
 // DB SETTINGS
 const sequelize = new Sequelize(
-    'brazuelo',
-    'brazuelo',
-    'soamee1pt',
-    // process.env.DB_NAME,
-    // process.env.DB_USER,
-    // process.env.DB_PASS,
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
     {
         host: 'db4free.net',
         dialect: 'mysql',
     });
-// console.log(process.env.DB_NAME);
+console.log(process.env.DB_NAME);
 module.exports = sequelize;
